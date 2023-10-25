06f756849ab0970c25c86fb2018a46bdd416af38
Elco Klingen <disillusion84@gmail.com>
2023-10-25 18:41:50 +0200

    initial release

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# vinyl-stream-prettier
-Small vinyl-stream wrapper -aka Gulp plugin- for prettier.
+# Small vinyl-stream wrapper -aka Gulp plugin- for prettier
+
+Run prettier within your streams. Supports automatic fixing of files. Also has a fast method for more speed, and a slow method for more control.
+
+> _NOTE:_ No tests have been written yet!
+
+## Installation
+
+`yarn install`. Or `npm install`. Or just copy the files to your own project.
+
+## Usage
+
+```javascript
+const prettierWrapper = require("@eklingen/vinyl-stream-prettier");
+stream.pipe(prettierWrapper());
+```
+
+This package assumes a configuration dotfile where prettier can find it.
+
+## Options
+
+You have the following options:
+
+### `failAfterError`
+
+This will determine wether to fail or not. Useful in a pre-commit hook, for example.
+
+```javascript
+prettierWrapper({
+  failAfterError: true,
+});
+```
+
+### `prettier`
+
+These options are passed verbatim to prettier. See the ["prettier"](https://www.npmjs.com/packages/prettier) documentation for more details.
+
+```javascript
+prettierWrapper({
+  prettier: {
+    fix: false,
+  },
+});
+```
+
+## Dependencies
+
+This package requires ["prettier"](https://www.npmjs.com/package/prettier).
+
+---
+
+Copyright (c) 2023 Elco Klingen. MIT License.
---
+++ index.js
@@ -0,0 +1,84 @@
+// Small vinyl-stream wrapper -aka Gulp plugin- for prettier.
+// Supports autofixing of files.
+
+const { relative } = require("path");
+const { Transform } = require("stream");
+
+const DEFAULT_OPTIONS = {
+  failAfterError: false,
+  prettier: {
+    useCache: true,
+    fix: true,
+  },
+};
+
+function prettierWrapper(options = {}) {
+  options = { ...DEFAULT_OPTIONS, ...options };
+  options.prettier = { ...DEFAULT_OPTIONS.prettier, ...options.prettier };
+
+  const prettier = require("prettier");
+  const unformattedFiles = [];
+
+  async function transform(file, encoding, callback) {
+    if (!file.isBuffer() || !file.contents || !file.contents.length) {
+      return;
+    }
+
+    const config = await prettier.resolveConfig(file.path, options.prettier);
+    const fileOptions = { ...config, ...options.prettier, filepath: file.path };
+    const contents = file.contents.toString("utf8");
+
+    try {
+      if (options.prettier.fix) {
+        const formattedCode = await prettier.format(contents, fileOptions);
+
+        if (formattedCode !== contents) {
+          file.isPrettier = true;
+          file.contents = Buffer.from(formattedCode);
+
+          return callback(null, file);
+        }
+      } else {
+        const isFormatted = await prettier.check(contents, fileOptions);
+
+        if (!isFormatted) {
+          const filename = relative(process.cwd(), file.path).replace(
+            /\\/g,
+            "/",
+          );
+          unformattedFiles.push(filename);
+        }
+      }
+    } catch (error) {
+      return callback(error);
+    }
+
+    return callback();
+  }
+
+  function flush(callback) {
+    if (unformattedFiles.length > 0) {
+      const header =
+        "Code style issues found in the following file(s). Forgot to run Prettier?";
+      const body = unformattedFiles.join("\n");
+      const message = `\n${header}\n${body}\n`;
+
+      if (options.failAfterError) {
+        return callback(new Error(message));
+      } else {
+        console.log(message);
+      }
+    }
+
+    return callback();
+  }
+
+  return new Transform({
+    transform,
+    flush,
+    readableObjectMode: true,
+    writableObjectMode: true,
+  });
+}
+
+module.exports = prettierWrapper;
